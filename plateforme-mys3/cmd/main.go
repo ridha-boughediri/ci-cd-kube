1b60f037b3887421663acf06c1c89a77c1a7ebbc
ridha-boughediri <ridha.boughediri@laplateforme.io>
2024-10-03 13:06:45 +0200

    update main

--- plateforme-mys3/cmd/main.go
+++ plateforme-mys3/cmd/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"io"
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 	"strings"
 )
 
@@ -11,7 +13,6 @@ type Config struct {
 	AccessKeyID     string
 	SecretAccessKey string
 	Region          string
-	// autres configurations
 }
 
 // Fonction principale
@@ -23,32 +24,70 @@ func main() {
 	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// Normaliser le chemin
-		path := strings.TrimSuffix(r.URL.Path, "/")
-		bucketName := strings.TrimPrefix(path, "/")
+		handleRequest(w, r, cfg)
+	})
 
-		log.Printf("Requête reçue: %s %s", r.Method, bucketName)
+	log.Println("Serveur démarré sur le port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
 
+// handleRequest gère les différentes requêtes HTTP
+func handleRequest(w http.ResponseWriter, r *http.Request, cfg Config) {
+	// Normaliser le chemin
+	path := strings.TrimSuffix(r.URL.Path, "/")
+	splitPath := strings.Split(path, "/")
+
+	// Gestion des buckets
+	if len(splitPath) == 2 {
+		bucketName := splitPath[1]
 		switch r.Method {
 		case http.MethodPut:
 			createBucket(w, r, bucketName, cfg)
 		case http.MethodGet:
 			listBuckets(w, r, cfg)
+		case http.MethodHead:
+			w.WriteHeader(http.StatusOK)
+		case http.MethodOptions:
+			w.Header().Set("Allow", "PUT, GET, HEAD, OPTIONS")
+			w.WriteHeader(http.StatusOK)
 		default:
 			http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 		}
-	})
+		return
+	}
 
-	log.Println("Serveur démarré sur le port 8000S")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	// Gestion des objets dans les buckets
+	if len(splitPath) == 3 {
+		bucketName := splitPath[1]
+		objectName := splitPath[2]
+
+		switch r.Method {
+		case http.MethodPut:
+			uploadObject(w, r, bucketName, objectName, cfg)
+		case http.MethodGet:
+			downloadObject(w, r, bucketName, objectName, cfg)
+		case http.MethodDelete:
+			deleteObject(w, r, bucketName, objectName, cfg)
+		default:
+			http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		}
+		return
+	}
+
+	// Gestion des listes d'objets dans un bucket
+	if len(splitPath) == 3 && splitPath[2] == "objects" {
+		bucketName := splitPath[1]
+		if r.Method == http.MethodGet {
+			listObjects(w, r, bucketName, cfg)
+		} else {
+			http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		}
+		return
+	}
 }
 
 // Fonction pour créer un bucket
 func createBucket(w http.ResponseWriter, r *http.Request, bucketName string, cfg Config) {
-	// Vérifier l'authentification si nécessaire
-	// Ici, on suppose que l'authentification est désactivée
-
-	// Créer le répertoire pour le bucket
 	path := "./data/" + bucketName
 	err := os.Mkdir(path, 0755)
 	if err != nil {
@@ -67,7 +106,6 @@ func createBucket(w http.ResponseWriter, r *http.Request, bucketName string, cfg
 
 // Fonction pour lister les buckets
 func listBuckets(w http.ResponseWriter, r *http.Request, cfg Config) {
-	// Exemple simple sans authentification
 	dataDir := "./data/"
 	entries, err := os.ReadDir(dataDir)
 	if err != nil {
@@ -76,7 +114,6 @@ func listBuckets(w http.ResponseWriter, r *http.Request, cfg Config) {
 		return
 	}
 
-	// Construire une réponse XML simple
 	w.Header().Set("Content-Type", "application/xml")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("<ListAllMyBucketsResult xmlns=\"http://s3.amazonaws.com/doc/2006-03-01/\">"))
@@ -87,4 +124,93 @@ func listBuckets(w http.ResponseWriter, r *http.Request, cfg Config) {
 		}
 	}
 	w.Write([]byte("</Buckets></ListAllMyBucketsResult>"))
+}
+
+// Fonction pour uploader un objet
+func uploadObject(w http.ResponseWriter, r *http.Request, bucketName, objectName string, cfg Config) {
+	bucketPath := "./data/" + bucketName
+	if _, err := os.Stat(bucketPath); os.IsNotExist(err) {
+		http.Error(w, "Bucket Not Found", http.StatusNotFound)
+		return
+	}
+
+	objectPath := filepath.Join(bucketPath, objectName)
+	file, err := os.Create(objectPath)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	defer file.Close()
+
+	_, err = io.Copy(file, r.Body)
+	if err != nil {
+		http.Error(w, "Error Writing File", http.StatusInternalServerError)
+		return
+	}
+
+	log.Printf("Objet %s uploadé dans le bucket %s", objectName, bucketName)
+	w.WriteHeader(http.StatusOK)
+}
+
+// Fonction pour lister les objets dans un bucket
+func listObjects(w http.ResponseWriter, r *http.Request, bucketName string, cfg Config) {
+	bucketPath := "./data/" + bucketName
+	entries, err := os.ReadDir(bucketPath)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		log.Printf("Erreur lors de la lecture du répertoire bucket %s : %v", bucketName, err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/xml")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("<ListBucketResult xmlns=\"http://s3.amazonaws.com/doc/2006-03-01/\">"))
+	w.Write([]byte("<Objects>"))
+	for _, entry := range entries {
+		if !entry.IsDir() {
+			w.Write([]byte("<Object><Name>" + entry.Name() + "</Name></Object>"))
+		}
+	}
+	w.Write([]byte("</Objects></ListBucketResult>"))
+}
+
+// Fonction pour télécharger un objet
+func downloadObject(w http.ResponseWriter, r *http.Request, bucketName, objectName string, cfg Config) {
+	objectPath := filepath.Join("./data/", bucketName, objectName)
+	file, err := os.Open(objectPath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			http.Error(w, "Object Not Found", http.StatusNotFound)
+			return
+		}
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	defer file.Close()
+
+	w.Header().Set("Content-Type", "application/octet-stream")
+	_, err = io.Copy(w, file)
+	if err != nil {
+		http.Error(w, "Error Sending File", http.StatusInternalServerError)
+		return
+	}
+
+	log.Printf("Objet %s téléchargé depuis le bucket %s", objectName, bucketName)
+}
+
+// Fonction pour supprimer un objet
+func deleteObject(w http.ResponseWriter, r *http.Request, bucketName, objectName string, cfg Config) {
+	objectPath := filepath.Join("./data/", bucketName, objectName)
+	err := os.Remove(objectPath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			http.Error(w, "Object Not Found", http.StatusNotFound)
+			return
+		}
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	log.Printf("Objet %s supprimé du bucket %s", objectName, bucketName)
+	w.WriteHeader(http.StatusOK)
 }
--- plateforme-mys3/cmd/main_test.go
+++ plateforme-mys3/cmd/main_test.go
@@ -1,21 +1,30 @@
 package main
 
 import (
+	"bytes"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"os"
+	"path/filepath"
 	"strings"
 	"testing"
 )
 
 // setup : Crée le répertoire ./data/ avant les tests
 func setup() {
-	os.Mkdir("./data", 0755)
+	err := os.Mkdir("./data", 0755)
+	if err != nil && !os.IsExist(err) {
+		panic(err)
+	}
 }
 
 // teardown : Supprime le répertoire ./data/ après les tests
 func teardown() {
-	os.RemoveAll("./data")
+	err := os.RemoveAll("./data")
+	if err != nil {
+		panic(err)
+	}
 }
 
 // TestMain est utilisé pour configurer et nettoyer l'environnement des tests
@@ -33,40 +42,98 @@ func TestMain(m *testing.M) {
 	os.Exit(code)
 }
 
-// Test de la fonction createBucket
-func TestCreateBucket(t *testing.T) {
-	req := httptest.NewRequest(http.MethodPut, "/testbucket", nil)
+// Test de la fonction uploadObject
+func TestUploadObject(t *testing.T) {
+	// Créer un bucket pour le test
+	os.Mkdir("./data/testbucket", 0755)
+
+	// Simuler l'upload d'un objet
+	req := httptest.NewRequest(http.MethodPut, "/testbucket/testobject.txt", bytes.NewReader([]byte("Contenu de l'objet")))
 	w := httptest.NewRecorder()
 
-	createBucket(w, req, "testbucket", Config{})
+	uploadObject(w, req, "testbucket", "testobject.txt", Config{})
 
 	resp := w.Result()
 	if resp.StatusCode != http.StatusOK {
 		t.Errorf("Expected status %d but got %d", http.StatusOK, resp.StatusCode)
 	}
 
-	if _, err := os.Stat("./data/testbucket"); os.IsNotExist(err) {
-		t.Errorf("Expected bucket to be created, but it was not")
+	// Vérifier que l'objet a bien été créé dans le répertoire du bucket
+	objectPath := filepath.Join("./data/testbucket", "testobject.txt")
+	if _, err := os.Stat(objectPath); os.IsNotExist(err) {
+		t.Errorf("Expected object to be created, but it was not")
 	}
 }
 
-// Test de la fonction listBuckets
-func TestListBuckets(t *testing.T) {
-	os.Mkdir("./data/bucket1", 0755)
-	os.Mkdir("./data/bucket2", 0755)
+// Test de la fonction listObjects
+func TestListObjects(t *testing.T) {
+	// Créer un bucket et des objets fictifs pour le test
+	os.Mkdir("./data/testbucket", 0755)
+	os.WriteFile("./data/testbucket/object1.txt", []byte("contenu1"), 0644)
+	os.WriteFile("./data/testbucket/object2.txt", []byte("contenu2"), 0644)
 
-	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	// Simuler la requête GET pour lister les objets dans le bucket
+	req := httptest.NewRequest(http.MethodGet, "/testbucket/objects", nil)
 	w := httptest.NewRecorder()
 
-	listBuckets(w, req, Config{})
+	listObjects(w, req, "testbucket", Config{})
 
 	resp := w.Result()
 	if resp.StatusCode != http.StatusOK {
 		t.Errorf("Expected status %d but got %d", http.StatusOK, resp.StatusCode)
 	}
 
+	// Vérifier que les objets sont listés dans la réponse
 	body := w.Body.String()
-	if !strings.Contains(body, "<Name>bucket1</Name>") || !strings.Contains(body, "<Name>bucket2</Name>") {
-		t.Errorf("Expected buckets to be listed in response")
+	if !strings.Contains(body, "<Name>object1.txt</Name>") || !strings.Contains(body, "<Name>object2.txt</Name>") {
+		t.Errorf("Expected objects to be listed in response")
+	}
+}
+
+// Test de la fonction downloadObject
+func TestDownloadObject(t *testing.T) {
+	// Créer un bucket et un objet pour le test
+	os.Mkdir("./data/testbucket", 0755)
+	os.WriteFile("./data/testbucket/testobject.txt", []byte("Contenu de l'objet"), 0644)
+
+	// Simuler la requête GET pour télécharger l'objet
+	req := httptest.NewRequest(http.MethodGet, "/testbucket/testobject.txt", nil)
+	w := httptest.NewRecorder()
+
+	downloadObject(w, req, "testbucket", "testobject.txt", Config{})
+
+	resp := w.Result()
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("Expected status %d but got %d", http.StatusOK, resp.StatusCode)
+	}
+
+	// Vérifier que le contenu téléchargé est correct
+	body, _ := io.ReadAll(resp.Body)
+	if string(body) != "Contenu de l'objet" {
+		t.Errorf("Expected content %q but got %q", "Contenu de l'objet", string(body))
+	}
+}
+
+// Test de la fonction deleteObject
+func TestDeleteObject(t *testing.T) {
+	// Créer un bucket et un objet pour le test
+	os.Mkdir("./data/testbucket", 0755)
+	os.WriteFile("./data/testbucket/testobject.txt", []byte("Contenu de l'objet"), 0644)
+
+	// Simuler la requête DELETE pour supprimer l'objet
+	req := httptest.NewRequest(http.MethodDelete, "/testbucket/testobject.txt", nil)
+	w := httptest.NewRecorder()
+
+	deleteObject(w, req, "testbucket", "testobject.txt", Config{})
+
+	resp := w.Result()
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("Expected status %d but got %d", http.StatusOK, resp.StatusCode)
+	}
+
+	// Vérifier que l'objet a bien été supprimé
+	objectPath := filepath.Join("./data/testbucket", "testobject.txt")
+	if _, err := os.Stat(objectPath); !os.IsNotExist(err) {
+		t.Errorf("Expected object to be deleted, but it still exists")
 	}
 }
